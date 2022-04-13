fbfcfb0a61dd5390ad681d6fe4019549dfc7f507
remingtondavids <53348044+remingtondavids@users.noreply.github.com>
2022-04-13 02:24:22 -0400

    practicing some basic coding

---
+++ practice.ts
@@ -0,0 +1,38 @@
+
+
+
+var i = "Hello World";
+
+
+console.log(i);
+
+
+
+
+var arr = ["a", "b", "c"];
+var arr2 = ["d", "e", "f"];
+
+var arr3 = [arr, arr2];
+
+console.log(arr3[0][0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
