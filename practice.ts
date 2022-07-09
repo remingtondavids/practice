aec55af00bbe77c48254e72d69e09825ff473326
remingtondavids <53348044+remingtondavids@users.noreply.github.com>
2022-07-09 19:00:40 -0400

    Clear old content and add info about data types

--- practice.ts
+++ practice.ts
@@ -1,16 +1,97 @@
-var a = 4
+/*
+
+DATA TYPES
+
+1. Boolean
+2. Number
+3. String
+4. Array
+5. Tuple
+6. Enum
+7. Unknown
+8. Any
+9. Void
+10. Null and Undefined
+
+*/
 
 
-var poop = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ]
+/*
+
+Boolean
+Can be "true" or "false"
+
+*/
+
+let isMemeFunny: boolean = true;
+
+/*
+
+Number
+
+*/
+
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
 
 
-for (var i = 0; i < poop.length; i++) {
-    
-    if ((i % 2) == 1) {
-        console.log(poop[i])
-    }
+let myNum: number = 0xFF;
 
+let myNum2: number = 0b1010;
+
+let num: number = 1.3455;
+
+let myname: string = "remington"
+
+/*
+
+String
+
+*/
+
+let myString: string = "Hello World";
+
+let stingsdsds: string = 'Hello World';
+
+let templateString: string = `I am ${num} years old!
+
+My name is ${myname}`;
+
+
+//console.log("I am " + num + " years old!\n\nMy name is " + myname)
+
+
+
+
+console.log(templateString);
+
+/*
+
+Array
+
+*/
+
+let array = [];
+
+let anotherArray: number[] = [1, 2, 3];
+
+let yetAnotherArray = {
+    0: 1,
+    1: 2,
+    2: 3,
+    3: 4,
 }
+
+
+let myTuple: [string, number]
+
+myTuple = ["rem", 32]
+
+
+
+
 
 
 
